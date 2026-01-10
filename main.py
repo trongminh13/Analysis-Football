ece4459ac25b43888f286252432180fbb7d5b620
Minh2255 <80587574+trongminh13@users.noreply.github.com>
2026-01-11 00:18:42 +0700

    feature: check number array in id

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from utils import read_video, save_video
+from trackers import Tracker
 def main():
-    print('hello world')
+    #Read Video
+    video_frames = read_video('input_videos/football-video.mp4')
+
+    # Initialize Tracker
+    tracker = Tracker('models/best.pt')
+    tracks = tracker.get_objects_tracks(video_frames)
+    #Save video
+    save_video(video_frames, 'output_videos/output_video.avi')
 
 if __name__ == '__main__':
     main()
