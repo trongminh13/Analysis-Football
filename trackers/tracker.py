7b62f6be737c87ee660b26c5e05b6cded4937671
Minh2255 <80587574+trongminh13@users.noreply.github.com>
2026-01-14 04:06:17 +0700

    feature: Complete the team control feature.

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -5,6 +5,7 @@ from ultralytics import YOLO
 import supervision as sv
 import pickle
 import os
+import numpy
 import sys
 import pandas as pd
 sys.path.append('../')
@@ -158,8 +159,24 @@ class Tracker:
 
         return frame
 
+    def draw_team_ball_control(self,frame,frame_num,team_ball_control):
+        # Draw a semi-transparent rectaggle
+        overlay = frame.copy()
+        cv2.rectangle(overlay,(1350,850),(1900,970),(255,255,255),-1)
+        alpha = 0.4
+        cv2.addWeighted(overlay,alpha,frame,1-alpha,0,frame)
 
-    def draw_annotations(self,video_frames,tracks):
+        team_ball_control_till_frame = team_ball_control[:frame_num+1]
+        # Get the number of time team had ball control
+        team_1_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==1].shape[0]
+        team_2_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==2].shape[0]
+        team_1 = team_1_num_frames/(team_1_num_frames+team_2_num_frames)
+        team_2 = team_2_num_frames/(team_1_num_frames+team_2_num_frames)
+        cv2.putText(frame,f"Team 1 Ball Control:{team_1*100:.2f}%",(1400,900),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),3)
+        cv2.putText(frame,f"Team 2 Ball Control:{team_2*100:.2f}%",(1400,950),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),3)
+
+        return frame
+    def draw_annotations(self,video_frames,tracks,team_ball_control):
         output_video_frames = []
         print(f"Tổng số frame video: {len(video_frames)}")
         print(f"Tổng số frame trong data players: {len(tracks['players'])}")
@@ -180,6 +197,9 @@ class Tracker:
                 color = player.get("team_color",(0,0,225))
                 frame = self.draw_ellipse(frame, player["bbox"],color ,track_id)
 
+                if player.get('has_ball', False):
+                    frame = self.draw_traingle(frame, player["bbox"],(0,0,255))
+
             # draw referee
             for _, referee in referee_dict.items():
                 frame = self.draw_ellipse(frame, referee["bbox"], (0, 255, 255))
@@ -188,6 +208,8 @@ class Tracker:
             for track_id, ball in ball_dict.items():
                 frame = self.draw_traingle(frame, ball["bbox"],(0,225,0))
 
+            # Draw Team Ball Control
+            frame = self.draw_team_ball_control(frame,frame_num,team_ball_control)
             output_video_frames.append(frame)
 
         return output_video_frames
