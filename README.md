a98d7757ee4a07ad6de0c1193f3e4ea097acc93e
Minh2255 <80587574+trongminh13@users.noreply.github.com>
2026-01-15 10:34:28 +0700

    feature: Complete the speed_and_distance_estimator anh view_transformer

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Analysis-Football
+
+- Dự án phân tích tran bóng đá gồm phân tích 
+# Cách tải và chạy dự án này
+- Chạy những file ipynb
+- đưa file best.pt và last.pt vào models
+- chạy main
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator
 from view_transformer import ViewTransformer
+from speed_and_distance_estimator import SpeedAndDistance_Estimator
 def main():
     #Read Video
     video_frames = read_video('input_videos/football-video.mp4')
@@ -25,8 +26,15 @@ def main():
                                                                               stub_path='stubs/camera_movement_stub.pkl')
     camera_movement_estimator.add_adjust_positions_to_tracks(tracks,camera_movement_per_frame)
 
+    #View Transnformer
+    view_transformer = ViewTransformer()
+    view_transformer.add_transformed_position_to_tracks(tracks)
     # Interpolate Ball Positions
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+
+    # Speed and distance estimator
+    speed_and_distance_estimator = SpeedAndDistance_Estimator()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
 
     # Assign Player Teams
     team_assigner = TeamAssigner()
@@ -75,6 +83,9 @@ def main():
 
     #Draw Camera movement
     output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames,camera_movement_per_frame)
+
+    ##Draw speed and distance
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames,tracks)
     #Save video
     save_video(output_video_frames, 'output_videos/output_video.avi')
 
